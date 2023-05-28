ef67ff4851a8e242c8f5ff0414ca539e158d530f
admirdemir <admirdemir@gmail.com>
2023-05-28 22:56:27 +0200

    insta search implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,73 @@
-import './App.css';
+import algoliasearch from "algoliasearch/lite";
+import {
+  Configure,
+  Highlight,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+  SearchBox,
+} from "react-instantsearch-hooks-web";
 
-export function App() {
-  return <div></div>;
+import { Panel } from "./components/Panel";
+
+import type { Hit } from "instantsearch.js";
+
+import "./App.css";
+
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+export default function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="header-title">
+          <a href="/">react-instantsearch-hooks-app</a>
+        </h1>
+        <p className="header-subtitle"></p>
+      </header>
+
+      <div className="container">
+        <InstantSearch searchClient={searchClient} indexName="instant_search">
+          <Configure hitsPerPage={8} />
+          <div className="search-panel">
+            <div className="search-panel__filters">
+              <Panel header="brand">
+                <RefinementList attribute="brand" />
+              </Panel>
+            </div>
+
+            <div className="search-panel__results">
+              <SearchBox placeholder="" className="searchbox" />
+              <Hits hitComponent={Hit} />
+
+              <div className="pagination">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
+    </div>
+  );
+}
+
+type HitProps = {
+  hit: Hit;
+};
+
+function Hit({ hit }: HitProps) {
+  return (
+    <article>
+      <h1>
+        <Highlight attribute="name" hit={hit} />
+      </h1>
+      <p>
+        <Highlight attribute="description" hit={hit} />
+      </p>
+    </article>
+  );
 }
